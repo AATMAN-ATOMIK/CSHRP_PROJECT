534b16562b93423e29827a8973c415869554d7f4
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-02-11 14:01:51 +0530

    Add files via upload

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //One Dimen Array
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string val in cars)
+            {
+                Console.WriteLine(val);
+            }
+            Console.Read();
+        }
+    }
+}
